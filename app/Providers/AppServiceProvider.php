56122ec40c65ef4bc1bd57fd2d7f99efe01a46d8
BekaMan95 <bsahle95@gmail.com>
2025-02-26 21:12:59 +0300

    force https correct path function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (App::environment('production')) {
-            URL::forcScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
